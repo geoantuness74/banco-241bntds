c985b5e1da4460b29f7eea19b6ebdc886d70ec83
Durvaldo Gonçalves Marques <durvaldomarques@gmail.com>
2024-11-14 22:20:31 -0300

    Add files via upload

--- script_usuarios.js
+++ script_usuarios.js
@@ -17,3 +17,48 @@ for (let index = 0; index < usuarios.length; index++) {
     tabela.innerHTML += linha
 }
 
+const formulario_cadastro = document.getElementById("cadastro")
+
+formulario_cadastro.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const senhaDigitada = document.getElementById("senha-cadastro").value
+    const confirmacaoSenha = document.getElementById("senha-cadastro-2").value 
+    const emailDigitado = document.getElementById("email-cadastro").value
+
+    console.log(senhaDigitada, confirmacaoSenha, emailDigitado)
+
+    if (senhaDigitada != confirmacaoSenha) {
+        // Alerta de erro
+        Swal.fire("As senhas devem ser iguais");
+        // Quebrar a função
+        return
+    }
+
+    const hasEmailCadastrado = procuraUsuarioByEmail(emailDigitado)
+    if (hasEmailCadastrado) {
+        // alerta erro
+        Swal.fire("Já existe esse email no nosso banco de dados");
+        // Quebra funçao
+        return
+    }
+
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"))
+    const ultimoID = usuarios[usuarios.length -1]?.id || 0
+    const usuarioAdd = {
+        id: ultimoID + 1,
+        email: emailDigitado,
+        senha: senhaDigitada
+    }
+    
+    usuarios.push(usuarioAdd)
+    localStorage.setItem('usuarios', JSON.stringify(usuarios))
+})
+
+function procuraUsuarioByEmail (emailDigitado) {
+    const usuarios = JSON.parse(localStorage.getItem("usuarios"))
+    const found = usuarios.find((usuario) => {
+        return usuario.email == emailDigitado 
+    })
+    return found
+}
+
